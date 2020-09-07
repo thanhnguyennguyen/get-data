fdfaf59f5fbec5a667f39b5bf1977f363d6cb3cb
NguyenNguyen <nguyenbk92@gmail.com>
2020-09-07 15:16:34 +0700

    add data to request

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const axios = require('axios')
-function getData(url, method = '', timeout = 2000) {
+function getData(url, method = '', data = {} , timeout = 2000) {
 	return axios({
         url: url,
         method: method,
+	data: data,
         timeout: timeout,
         headers: {
             'Content-Type': 'application/json',
         }
     })
 }
-
 module.exports = getData
 
